ed931149dae0b6ccdd429885c302dd8ef8e0b304
billy5982 <kuu0404@naver.com>
2022-08-31 23:23:42 +0900

    2주차 1330번 완

---
+++ mk/week2/1_1330.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const input = fs.readFileSync(process.platform === "linux"? "/dev/stdin": __filename.split("/").pop().slice(0, -3) + ".txt" ).toString()
+.split(' ').map((el)=>Number(el))
+
+const [A,B] = input
+A>B&&console.log('>')
+A<B&&console.log('<')
+A===B&&console.log('==')
