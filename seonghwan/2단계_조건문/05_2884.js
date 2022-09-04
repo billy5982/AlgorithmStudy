6c96425411c59ace9722ca8032bf63cd4f0e173b
tlsrb100 <104765779+tlsrb100@users.noreply.github.com>
2022-09-04 22:19:53 +0900

    2주차 2884번 완료

---
+++ seonghwan/2단계_조건문/05_2884.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync(process.platform === 'linux' ? 'dev/stdin' : 'test.txt').toString().split(' ').map(Number);
+
+const [hour, min] = input;
+const convertedTime = hour*60 + min - 45;
+
+if(convertedTime < 0){
+  console.log( `23 ${convertedTime+60}`);
+}else{
+  console.log( `${Math.floor(convertedTime/60)} ${convertedTime%60}` );
+}
