58b1167d95e2bed2206bc5844a52eb4ab3fe6e0e
sharon-youn <jos9187@naver.com>
2022-09-07 08:44:56 +0900

    3주차 10950번 수정

--- sharon-yoon(Runjs)/week3/02_10950.js
+++ sharon-yoon(Runjs)/week3/02_10950.js
@@ -4,7 +4,7 @@ const fs = require("fs");
  // node.js의 fs 모듈을 사용해 파일을 읽어온다
 // 출력이 다음 줄에 있을땐 split('\n')으로 하고 
 // 옆에 있을떈 split(' ')로 하기!! 
-const input= require('fs').readFileSync('예제.txt').toString().split('\n')
+const input= require('fs').readFileSync('dev/stdin').toString().split('\n')
 //require('fs').readFileSync('dev/stdin')
 
 // 5
