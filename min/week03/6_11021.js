133e89a76cf45136bb4b8fa822cbe55845a5b04c
ALSRUD29 <dev.mnkg@gmail.com>
2022-09-13 19:02:22 +0900

    3주차 10952, 10951, 11022, 2438, 2439

--- min/week03/6_11021.js
+++ min/week03/6_11021.js
@@ -4,3 +4,11 @@ let answer = ''
 for(let i=1; i<= Number(input[0]); i++){
     console.log(`Case #${i}:`, Number(input[i].split(" ")[0])+Number(input[i].split(" ")[1]) )
 };
+
+//
+const [N, ...arr] = require('fs').readFileSync('./dev/stdin').toString().trim().split('\n');
+
+for (let i=0; i<N; i++) {
+  const [A, B] = arr[i].split(' ').map(Number);
+  console.log(`Case #${i+1}:`, A + B);
+}
